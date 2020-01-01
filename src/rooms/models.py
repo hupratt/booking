2e27ac028022175b02d201f258e4a9c0a09d08a6
hupratt <hprattdo@gmail.com>
2020-01-01 17:06:56 +0100

    better backend: refactor

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from schedule.models import Calendar
 import os
-
+from locations.models import Location
+from schedule.models import CalendarRelation
 # Create your models here.
 
 class Room(Calendar):
@@ -18,6 +19,20 @@ class Room(Calendar):
     double_bed_number = models.PositiveSmallIntegerField()
     bath_number = models.PositiveSmallIntegerField()
     surface_area_sqmeter = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=120, null=True, blank=True)
+
+    def get_parent_location(self):
+        calendarRelation_qs = CalendarRelation.objects.filter(calendar_id=self.id)
+        if len(calendarRelation_qs)>1:
+            return None
+        parent_location_id = calendarRelation_qs[0].object_id
+        parent_location = Location.objects.filter(id=parent_location_id)
+        if len(parent_location)>1:
+            raise ValueError("A room can only have one parent location")
+        return parent_location[0]
+
+    def get_room_images(self):
+        return RoomImage.objects.filter(location_id=self.id)
 
 class RoomImage(models.Model):
     image = models.FileField( blank=True, null=True)
@@ -31,8 +46,5 @@ class RoomImage(models.Model):
     def __unicode__(self):
         return self.alt
 
-    @staticmethod
-    def get_image_path(instance):
-        return os.path.join(
-            "static", instance.id
-        )  
+
+
--- src/rooms/views.py
+++ src/rooms/views.py
@@ -1,13 +1,15 @@
 from schedule.views import (
     CalendarByPeriodsView
 )
-from rooms.models import RoomImage
+from rooms.models import RoomImage, Room
+from locations.models import Location
+from schedule.models import CalendarRelation
 
 class RoomView(CalendarByPeriodsView):
-
+    model = Room
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        calendar = self.object
-        room_images = RoomImage.objects.filter(location=calendar)
-        context["room_images"] = room_images
+        room = self.object
+        context["room_images"] = room.get_room_images()
+        context["parent_location"] = room.get_parent_location()
         return context
