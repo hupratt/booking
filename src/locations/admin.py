b98ccc38124dae554cea2abb1ebe182a205e69e1
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-01-02 18:10:09 +0100

    from function to decorator

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -4,8 +4,9 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
 from klingon.admin import TranslationInline, create_translations
+from django.contrib.sessions.models import Session
 
-# Register your models here.
+@admin.register(Location)
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ['title','timestamp','updated','tag','id','user']
     list_filter = ["updated",'timestamp']
@@ -15,21 +16,18 @@ class PostModelAdmin(admin.ModelAdmin):
     # actions = [create_translations]
     class Meta:
         model = Location
-admin.site.register(Location,PostModelAdmin)
 
-
+@admin.register(LocationImage)
 class PostImageModelAdmin(admin.ModelAdmin):
     list_display = ['image','location','created']
     class Meta:
         model = LocationImage
-admin.site.register(LocationImage,PostImageModelAdmin)
 
-from django.contrib.sessions.models import Session
+@admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
         return obj.get_decoded()
     list_display = ['session_key', '_session_data', 'expire_date']
-admin.site.register(Session, SessionAdmin)
 
 
 class MyUserChangeForm(UserChangeForm):
