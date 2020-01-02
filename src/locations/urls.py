4e28cf6e5a7c70238bbb534ba6cfd7562acb84b6
Hugo Pratt <38809698+hupratt@users.noreply.github.com>
2020-01-02 10:07:31 +0100

    Update urls.py

--- src/locations/urls.py
+++ src/locations/urls.py
@@ -3,14 +3,14 @@ from . import views
 # from django.conf import settings
 # from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.urls import path # pylint: disable=no-name-in-module
+from django.urls import path
 from . import views
 from schedule.periods import Day, Month, Week, Year
-from schedule.views import (
-    CalendarByPeriodsView
-)
+from schedule.views import CalendarByPeriodsView
+from django.utils.translation import ugettext_lazy as _
+
 urlpatterns = [
-    path('about', views.AboutView.as_view(), name='about'), 
-    path('<slug:slug>', views.detail, name="detail"),
+    path(_('about'), views.AboutView.as_view(), name='about'), 
+    path(_('<slug:slug>'), views.detail, name="detail"),
 
 ]
